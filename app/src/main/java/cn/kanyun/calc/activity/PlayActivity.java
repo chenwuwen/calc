82edb24b083bd12ffa3ddab7a59dc5aa805cd2fb
chenwuwen <git.123>
2019-08-09 14:06:34 +0800

    第一次提交

---
+++ app/src/main/java/cn/kanyun/calc/activity/PlayActivity.java
@@ -0,0 +1,16 @@
+package cn.kanyun.calc.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.kanyun.calc.R;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
